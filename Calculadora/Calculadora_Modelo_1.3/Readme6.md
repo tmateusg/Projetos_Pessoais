2d0af4eb7db8377221a398f532d43309b0aefee0
tmateus <tmateusguimaraes@bol.com.br>
2021-08-05 18:52:38 -0300

    inserido versão 1.3

---
+++ Calculadora/Calculadora_Modelo_1.3/Readme6.md
@@ -0,0 +1,10 @@
+# Calculadora 1.2
+### Esta calculadora executa contas aritmeticas simples de dois numeros.
+##**Pontos de melhoria:**
+#### A- Introduzido o comando **DEF** deixando os comandos de decisão (IF) mais limpos.
+#### B- Introduzido modulos deixando as funções divididas e o projeto com menos linhas.
+#### C- Unificando as fuções aritmeticas.
+
+1. Interfase.
+2. Aceitar só numeros inteiros.
+3. Operar com mais de 2 valores.
---
+++ Calculadora/Calculadora_Modelo_1.3/funcoes_DEF.py
@@ -0,0 +1,11 @@
+def operacoes(numero1, numero2):
+    soma = numero1 + numero2
+    subtracao = numero1 - numero2
+    multiplicacao = numero1 * numero2
+    divisao = numero1 / numero2
+
+    print(f'A soma de {numero1} + {numero2} é = \t\t\t{soma:.2f}')
+    print(f'A subtracao de {numero1} - {numero2} é = \t\t{subtracao:.2f}')
+    print(f'A multiplicacao de {numero1} * {numero2} é = {multiplicacao:.2f}')
+    print(f'A divisao de {numero1} / {numero2} é =\t\t{divisao:.2f}')
+    print('#' * 40)
